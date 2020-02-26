d43ebd30a8ddf4deeeece303ad54b10d9072f1db
jubs12 <julianarsg13@gmail.com>
2020-02-26 13:46:35 -0300

    mt-dnn/train.py command with custom args

---
+++ Training/run.sh
@@ -0,0 +1,8 @@
+python train.py \
+--do_lower_case \
+--log_file $1.log \
+--init_checkpoint mt_dnn_models/mt_dnn_base_uncased.pt \
+--task_def $1.yml \
+--train_datasets $1 \
+--test_datasets $1_train,$1_dev,$1_test \
+--train_datasets $1
