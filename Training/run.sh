ed568b8321cf1e7e795cf6accc7c3eb11192322f
jubs12 <julianarsg13@gmail.com>
2020-02-26 13:51:58 -0300

    typo

--- Training/run.sh
+++ Training/run.sh
@@ -2,7 +2,7 @@ python train.py \
 --do_lower_case \
 --log_file $1.log \
 --init_checkpoint mt_dnn_models/mt_dnn_base_uncased.pt \
---task_def $1.yml \
+--task_def $1.yaml \
 --train_datasets $1 \
 --test_datasets $1_train,$1_dev,$1_test \
 --train_datasets $1
