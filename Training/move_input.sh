c5ed696e854c553027797071e7971a88a3ec3eeb
jubs12 <julianarsg13@gmail.com>
2020-03-07 00:21:16 -0300

    training unified

--- Training/move_input.sh
+++ Training/move_input.sh
@@ -2,9 +2,9 @@
 
 if [[ $1 =~ assin ]];
 then
-    mv mtdnn_port/Training/assin/$1/*.tsv data/canonical_data/
-    mv mtdnn_port/Training/assin/$1/*.yaml ./
+    mv ../Training/assin/$1/*.tsv data/canonical_data/
+    mv ../Training/assin/$1/*.yaml ./
 else
-    mv mtdnn_port/Training/$1/*.tsv data/canonical_data/
-    mv mtdnn_port/Training/$1/*.yaml ./
+    mv ../Training/$1/*.tsv data/canonical_data/
+    mv ../Training/$1/*.yaml ./
 fi
