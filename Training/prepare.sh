a1b348e7f2327e6ea83b66da0b7875a9118bfc36
Juliana Resplande <julianarsg13@gmail.com>
2020-04-08 13:18:40 -0300

    Update prepare.sh

--- Training/prepare.sh
+++ Training/prepare.sh
@@ -20,18 +20,37 @@ then
     exit 127
 fi
 
+echo "Entering mt-dnn repository"
 git clone https://github.com/namisan/mt-dnn
 git checkout 60aa9dc4ec
 cd mt-dnn
+
+echo "Installing transformers"
 pip install transformers
-bash download.sh
+
+echo "Download mt-dnn repository models: [y/n]"
+echo "For bert and Portuguese bert, select n"
+echo "For mt-dnn, select y"
+read download
+
+if ["$download" = "y" ]; then
+    bash download.sh
+fi
+
+
+echo "Getting input data"
 cp ../move_assin.sh move_assin.sh
-mkdir data/canonical_data
+mkdir data data/canonical_data
 bash move_assin.sh $tweetsent $original
+
+echo "Patching files"
 patch data_utils/metrics.py < ../metrics.patch
 patch mt_dnn/model.py < ../model.patch
 patch prepro_std.py < ../prepro_std.patch
 patch train.py < ../train.patch
+
+echo "Preparing tasks definitions"
 cp ../task_defs.sh
 bash task_defs.sh
 echo "Please copy task_list" 
+echo "Also note the current work directory is $pwd"
