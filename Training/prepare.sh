7cc1d5a39f1db5c1e4645b3689058323388fd1b1
Juliana Resplande <julianarsg13@gmail.com>
2020-04-17 01:28:37 +0000

    mtl script corrections

--- Training/prepare.sh
+++ Training/prepare.sh
@@ -1,14 +1,14 @@
 echo "Entering mt-dnn repository"
 git clone https://github.com/namisan/mt-dnn
+cd mt-dnn
 git checkout f444fe9109d
-cd -v mt-dnn
 
 echo "Downloading mt-dnn repository models"
 bash download.sh
 
 echo "Getting input data"
 cp -v ../move_assin.sh move_assin.sh
-mkdir -v data data/canonical_data
+mkdir -v data/canonical_data
 bash move_assin.sh $1
 
 echo "Patching files"
@@ -16,7 +16,5 @@ patch train.py < ../train.patch
 patch data_utils/metrics.py < ../metrics.patch
 
 echo "Preparing tasks definitions"
-cp -v ../task_defs.sh
+cp -v ../task_defs.sh .
 bash task_defs.sh
-echo "Please copy task_list"
-echo "Also note the current work directory is $pwd"
