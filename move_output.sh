e3f198a7de511d4d7d6834d0a8f67fbdac65e322
Juliana Resplande <julianarsg13@gmail.com>
2020-06-08 15:22:25 -0300

    move dropout 0.01 common experiments

--- move_output.sh
+++ move_output.sh
@@ -1,25 +1,30 @@
 MODE=$1
 
+declare -a grad_norms=("1.0" "2.0" "5.0") 
 declare -a seeds=("2016" "2017" "2018" "2019" "2020")
-declare -a models=("bert-pt")
+declare -a models=("bert-pt" "bert" "mt-dnn")
 declare -a types=("base" "large")
 
 for MODEL in "${models[@]}"; do
     for TYPE in "${types[@]}"; do
         for SEED in "${seeds[@]}"; do
-            PREFIX=output/$MODE/${MODEL}_$TYPE/seed/$SEED
-            SUFIX=grad_norm/1.0/
+            for GRAD_NORM in "${grad_norms[@]}"; do
+                PREFIX=output/$MODE/${MODEL}_$TYPE/seed/$SEED/grad_norm/$GRAD_NORM
+                SUFIX=dropout/0.1/
             
-            mkdir -p $PREFIX/$SUFIX
-            cp $PREFIX/*.json $PREFIX/$SUFIX
+                mkdir -p $PREFIX/$SUFIX
+                cp $PREFIX/*.json $PREFIX/$SUFIX
+            done
         done
     done
 done
 
 for SEED in "${seeds[@]}"; do
-    PREFIX=output/$MODE/$TASK/bert-multilingual_base/seed/$SEED
-    SUFIX=grad_norm/1.0/
+    for GRAD_NORM in "${grad_norms[@]}"; do
+        PREFIX=output/$MODE/bert-multilingual_base/seed/$SEED/grad_norm/$GRAD_NORM
+        SUFIX=dropout/0.1/
     
-    mkdir -p $PREFIX/$SUFIX
-    cp $PREFIX/*.json $PREFIX/$SUFIX
+        mkdir -p $PREFIX/$SUFIX
+        cp $PREFIX/*.json $PREFIX/$SUFIX
+    done
 done
