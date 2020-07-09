f9f6affd5cb1c92d6e9397fac9af784ee257ac0e
Juliana Resplande <julianarsg13@gmail.com>
2020-07-09 15:16:55 -0300

    correct mean reports

--- mean_seed.py
+++ mean_seed.py
@@ -5,67 +5,52 @@ import os
 from typing import Iterable, List
 from pprint import pprint
 
-assin_report = \
-    r"""RTE evaluation
-Accuracy	Macro F1
---------	--------
-  (.....)%	   (.....)
+assin_idx = [[55, 60], [65, 70], [152, 157], [173, 177]]
+tweetsent_idx = [[6, 12], [22, 27], [33, 38], [45, 50]]
 
-Similarity evaluation
-Pearson		Mean Squared Error
--------		------------------
-  (.....)		              (....)"""
+def get_metric(task: str, metric_pos: int):
+    start, end = metric_pos[0], metric_pos[1] 
 
-tweetsent_report = \
-    r"""Acc: (......)
-F1. neg: (.....) neu: (.....) post: (.....)
-Acc dist min: (......)
-Acc dist max: (......)
-Acc dist min equal: (......)
-Acc dist max equal: (......)"""
+    return task[start:end]
 
-rounding_assin = [2, 3, 3, 2]
-rounding_tweetsent = [5, 3, 3, 3]
+def get_idx(task: str):
+    sample_metric = get_metric(task, assin_idx[0])
 
+    try:
+        float(sample_metric)
+        idx = assin_idx
+    except ValueError:
+        idx = tweetsent_idx
 
-def assin_match(task: str):
-    return re.search(assin_report, task)
-
-
-def tweetsent_match(task: str):
-    return re.search(tweetsent_report, task)
-
+    return idx
 
 def get_metrics(task: str) -> tuple:
-    match = assin_match(task) if assin_match(task) else tweetsent_match(task)
-
-    if not match:
-        raise ValueError('Task report not in ASSIN or TweetSentBR format')
-
-    metrics = [match.group(i) for i in range(1, 5)]
+    metric_idx = get_idx(task)
+    metrics = [get_metric(task, idx) for idx in metric_idx]
 
     return metrics
 
-
 def set_metrics(task: str, mean: tuple, std: tuple):
     symbol = '\u00b1'
-    rounding = rounding_assin if assin_match(task) else rounding_tweetsent
+    metric_idx = get_idx(task)
+    rounding = [2, 3, 3, 2] if metric_idx == assin_idx \
+               else [4, 3, 3, 3]
 
-    metrics_sample = tuple(get_metrics(task))
-    metrics_final = [
+    metrics = [
         str(mean[i].round(rounding[i])) +
         symbol +
         str(std[i].round(rounding[i]))
         for i in range(0, 4)
     ]
 
-    for idx, metric in enumerate(metrics_sample):
-        task = re.sub(metric, metrics_final[idx], task)
+    reversed_metrics = list(reversed(metrics))
+    reversed_idx = reversed(metric_idx)
 
-    #task = re.sub("\n  ", "\n", task)
-    #task = re.sub(r"	   ([0-9])", r"	\1", task)
-    #task = re.sub(r"		              ([0-9])", r"	\1", task)
+    for i, idx in enumerate(reversed_idx):
+        task = task[0: idx[0]] + reversed_metrics[i] + task[idx[1]:]
 
+    task = re.sub("		              ", "	   ", task)
+    
     return task
 
 
@@ -109,8 +94,8 @@ def save_text(txt: str, outpath: str):
 
 def main():
     path = 'report/seed'
-    pattern = 'seed_2(.*).txt'
-    outfile = 'seed_mean.txt'
+    pattern = 'cabezudo_2(.*).txt'
+    outfile = 'cabezudo_mean.txt'
 
     scores_lst = list()
 
